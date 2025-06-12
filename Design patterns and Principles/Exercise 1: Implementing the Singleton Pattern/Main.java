2814dbd73846af00e75a62910857115d47b7530b
sailumalkar <sailulakshmi2005@gmail.com>
2025-06-12 17:04:07 +0530

    Add files via upload

---
+++ Design patterns and Principles/Exercise 1: Implementing the Singleton Pattern/Main.java
@@ -0,0 +1,17 @@
+package SingletonPatternExample.src.singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        logger1.log("First message");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Second message");
+
+        if (logger1 == logger2) {
+            System.out.println("Both logger1 and logger2 are the same instance.");
+        } else {
+            System.out.println("Different instances exist (Singleton failed).");
+        }
+    }
+}
